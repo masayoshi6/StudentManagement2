138bf898ecabed915cb7a0b62738ca4150794ca8
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-04-01 11:24:06 +0900

    初回コミット

---
+++ src/test/java/raise/student/management2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package raise.student.management2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
